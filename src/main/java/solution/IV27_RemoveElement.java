fcd371a7c44f44660e8f3d84ec5ead4f3c90ed08
Tanitsak Jirawuttanakit <tanitsakjirawuttanakit@Tanitsaks-MacBook-Pro.local>
2024-01-18 21:50:56 +0700

    Add solution inside IV27_RemoveElement.java

--- src/main/java/solution/IV27_RemoveElement.java
+++ src/main/java/solution/IV27_RemoveElement.java
@@ -1,5 +1,14 @@
 package solution;
 
 public class IV27_RemoveElement {
-
+	public int removeElement(int[] nums, int val) {
+		int index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != val) {
+				nums[index] = nums[i];
+				index++;
+			}
+		}
+		return index;
+	}
 }
