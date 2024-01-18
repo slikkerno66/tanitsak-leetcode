c48d47957dc3513262fe05ef9e451fb0eeed7f61
Tanitsak <tanitsak.jir@gmail.com>
2024-01-18 20:25:58 +0700

    Add RotateArray189

---
+++ src/main/java/RotateArray189.java
@@ -0,0 +1,41 @@
+
+public class RotateArray189 {
+
+	public static void main(String[] args) {
+
+		int nums[] = { -1, -100, 3, 99 };
+		int k = 2;
+
+		RotateArray189 obj = new RotateArray189();
+		obj.rotate(nums, k);
+
+		// prove solution
+		obj.printArray(nums);
+
+	}
+
+	public void rotate(int[] nums, int k) {
+		int tempNums[] = new int[nums.length];
+
+		if (k != 0 && nums.length != 0 && nums.length != 1) {
+			for (int i = 0; i < nums.length; i++) {
+				int newPosition = i + k;
+
+				if (newPosition >= nums.length) {
+					newPosition = nums.length > newPosition ? nums.length % newPosition : newPosition % nums.length;
+				}
+
+				tempNums[newPosition] = nums[i];
+			}
+
+			System.arraycopy(tempNums, 0, nums, 0, tempNums.length);
+		}
+	}
+
+	public void printArray(int nums[]) {
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + " ");
+		}
+	}
+
+}
