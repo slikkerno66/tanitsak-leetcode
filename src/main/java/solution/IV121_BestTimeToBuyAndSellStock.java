f3321b762c8054c70a15108980f2e52381c819c9
Tanitsak Jirawuttanakit <tanitsakjirawuttanakit@Tanitsaks-MacBook-Pro.local>
2024-01-19 23:25:29 +0700

    Add IV121 and IV169

--- src/main/java/solution/IV121_BestTimeToBuyAndSellStock.java
+++ src/main/java/solution/IV121_BestTimeToBuyAndSellStock.java
@@ -3,7 +3,85 @@ package solution;
 public class IV121_BestTimeToBuyAndSellStock {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int prices[] = { 7, 1, 5, 3, 6, 4 };
+
+		IV121_BestTimeToBuyAndSellStock obj = new IV121_BestTimeToBuyAndSellStock();
+
+		obj.maxProfit(prices);
+
+	}
+
+// solution 1: doesn't work. Got time exceeded.
+//	public int maxProfit(int[] prices) {
+//
+//		int maxProfit = 0;
+//		int currentProfit = 0;
+//
+//		for (int i = 0; i < prices.length; i++) {
+//
+//			for (int j = i + 1; j < prices.length; j++) {
+//				if (prices[i] < prices[j]) {
+//					currentProfit = prices[j] - prices[i];
+//				}
+//
+//				if (maxProfit < currentProfit) {
+//					maxProfit = currentProfit;
+//				}
+//			}
+//
+//		}
+//
+//		System.out.println(maxProfit);
+//
+//		return maxProfit;
+//
+//	}
+
+//this solution got accepted
+//	public int maxProfit(int[] prices) {
+//
+//		int maxProfit = 0;
+//		int currentProfit = 0;
+//		int minimumPrice = prices[0];
+//
+//		if (prices.length > 0) {
+//			for (int i = 0; i < prices.length; i++) {
+//
+//				if (prices[i] < minimumPrice) {
+//					minimumPrice = prices[i];
+//				}
+//
+//				currentProfit = prices[i] - minimumPrice;
+//
+//				if (maxProfit < currentProfit) {
+//					maxProfit = currentProfit;
+//				}
+//
+//			}
+//
+//		}
+//
+//		System.out.println(maxProfit);
+//
+//		return maxProfit;
+//
+//	}
+
+	// shortest form
+	public int maxProfit(int[] prices) {
+
+		int minPrice = Integer.MAX_VALUE;
+		int maxProfit = 0;
+
+		for (int price : prices) {
+			minPrice = Math.min(minPrice, price);
+			maxProfit = Math.max(maxProfit, price - minPrice);
+		}
+
+		System.out.println(maxProfit);
+
+		return maxProfit;
 
 	}
 
---
+++ src/main/java/solution/IV169_MajorityElement.java
@@ -0,0 +1,43 @@
+package solution;
+
+import java.util.Arrays;
+
+public class IV169_MajorityElement {
+
+	public static void main(String args[]) {
+
+		int nums[] = { 3, 2, 3 };
+
+		IV169_MajorityElement obj = new IV169_MajorityElement();
+
+		obj.majorityElement(nums);
+	}
+
+	public int majorityElement(int[] nums) {
+
+		Arrays.sort(nums);
+
+		int majorNumber = nums[0];
+		int majorNumberCount = 0;
+
+		int currentNumber = majorNumber;
+		int currentNumberCount = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (currentNumber == nums[i]) {
+				currentNumberCount++;
+			}
+			if (currentNumber != nums[i] || i == nums.length - 1)
+				if (majorNumberCount == 0 || majorNumberCount < currentNumberCount) {
+					majorNumber = currentNumber;
+					majorNumberCount = currentNumberCount;
+					currentNumberCount = 1;
+
+				}
+			currentNumber = nums[i];
+		}
+
+		return majorNumber;
+
+	}
+}
