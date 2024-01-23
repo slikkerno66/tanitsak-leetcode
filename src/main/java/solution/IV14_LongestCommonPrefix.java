80f49b42b3d04287609a8ebfc41a384e790638dc
Tanitsak <tanitsak.jir@gmail.com>
2024-01-23 20:18:21 +0700

    Add IV14 and IV58

---
+++ src/main/java/solution/IV14_LongestCommonPrefix.java
@@ -0,0 +1,70 @@
+package solution;
+
+public class IV14_LongestCommonPrefix {
+
+	public static void main(String[] args) {
+
+		// "flower", "flow", "flight"
+		// "dog","racecar","car"
+		// "a"
+		// "ab", "a"
+		// "aaa","aa","aaa"
+		// "abab","aba",""
+
+		String[] s = { "flower", "flow", "flight" };
+
+		IV14_LongestCommonPrefix obj = new IV14_LongestCommonPrefix();
+
+		obj.longestCommonPrefix(s);
+	}
+
+	public String longestCommonPrefix(String[] strs) {
+		StringBuilder sb = new StringBuilder();
+
+		boolean keepDoing = true;
+		boolean notLastTime = true;
+
+		if (strs.length == 1)
+			sb.append(strs[0]);
+
+		for (int i = 0; i < strs[0].length(); i++) {
+
+			int j = 1;
+			while (j <= strs.length - 1 && keepDoing) {
+
+				if (strs[j].length() == 0) {
+					keepDoing = false;
+				} else {
+					if (strs[0].charAt(i) != strs[j].charAt(i)) {
+						keepDoing = false;
+					}
+
+					if (i == strs[j].length() - 1) {
+						notLastTime = false;
+					}
+
+					if (strs[0].charAt(i) == strs[j].charAt(i) && j == strs.length - 1) {
+
+						sb.append(strs[0].charAt(i));
+
+						if (i == strs[j].length() - 1 || !notLastTime) {
+							keepDoing = false;
+						}
+					}
+					j++;
+				}
+
+			}
+
+			if (!keepDoing) {
+				break;
+			}
+
+		}
+
+		System.out.println(sb.toString());
+
+		return sb.toString();
+	}
+
+}
---
+++ src/main/java/solution/IV58_LengthOfLastWord.java
@@ -0,0 +1,37 @@
+package solution;
+
+public class IV58_LengthOfLastWord {
+
+	public static void main(String[] args) {
+		String s = "a";
+
+		IV58_LengthOfLastWord obj = new IV58_LengthOfLastWord();
+		obj.lengthOfLastWord(s);
+	}
+
+// first version
+//	public int lengthOfLastWord(String s) {
+//
+//		String[] splited = s.split("\\s+");
+//
+//		return splited[splited.length - 1].length();
+//	}
+
+	// better solution
+	public int lengthOfLastWord(String s) {
+
+		s = s.trim();
+
+		int lastWordLength = 0;
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (s.charAt(i) == ' ') {
+				break;
+			}
+			lastWordLength++;
+		}
+
+		return lastWordLength;
+	}
+
+}
