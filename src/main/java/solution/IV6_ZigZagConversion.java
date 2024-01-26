70345ae0a60f44324344c699cc3cd3e3857baf50
Tanitsak <tanitsak.jir@gmail.com>
2024-01-26 11:45:55 +0700

    Add IV6 without logic

---
+++ src/main/java/solution/IV6_ZigZagConversion.java
@@ -0,0 +1,44 @@
+package solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IV6_ZigZagConversion {
+
+	public static void main(String[] args) {
+		String s = "PAYPALISHIRING";
+
+		IV6_ZigZagConversion obj = new IV6_ZigZagConversion();
+		obj.convert(s, 3);
+	}
+
+	public String convert(String s, int numRows) {
+
+//		List<List<Integer>> test = new ArrayList<>();
+//
+//		test.add(new ArrayList<>());
+//		test.add(new ArrayList<>());
+//
+//		test.get(0).add(1);
+//		test.get(0).add(2);
+//		test.get(0).add(3);
+//		test.get(0).add(4);
+//		test.get(0).add(5);
+//
+//		test.get(1).add(6);
+//		test.get(1).add(7);
+//		test.get(1).add(8);
+//		test.get(1).add(9);
+//		test.get(1).add(10);
+//
+//		for (int i = 0; i < test.size(); i++) {
+//			for (Integer item : test.get(i)) {
+//				System.out.print(item + " ");
+//			}
+//			System.out.println();
+//		}
+
+		return "";
+	}
+
+}
