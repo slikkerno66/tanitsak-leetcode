79d6f65c520900a280e19afe75466495f99de1ed
Tanitsak Jirawuttanakit <tanitsakjirawuttanakit@Tanitsaks-MacBook-Pro.local>
2024-01-26 14:50:45 +0700

    Add logic to IV6

--- src/main/java/solution/IV6_ZigZagConversion.java
+++ src/main/java/solution/IV6_ZigZagConversion.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class IV6_ZigZagConversion {
 
 	public static void main(String[] args) {
+		// PAYPALISHIRING
 		String s = "PAYPALISHIRING";
 
 		IV6_ZigZagConversion obj = new IV6_ZigZagConversion();
@@ -14,31 +15,57 @@ public class IV6_ZigZagConversion {
 
 	public String convert(String s, int numRows) {
 
-//		List<List<Integer>> test = new ArrayList<>();
-//
-//		test.add(new ArrayList<>());
-//		test.add(new ArrayList<>());
-//
-//		test.get(0).add(1);
-//		test.get(0).add(2);
-//		test.get(0).add(3);
-//		test.get(0).add(4);
-//		test.get(0).add(5);
-//
-//		test.get(1).add(6);
-//		test.get(1).add(7);
-//		test.get(1).add(8);
-//		test.get(1).add(9);
-//		test.get(1).add(10);
-//
-//		for (int i = 0; i < test.size(); i++) {
-//			for (Integer item : test.get(i)) {
-//				System.out.print(item + " ");
-//			}
-//			System.out.println();
-//		}
+		StringBuilder sb = new StringBuilder();
 
-		return "";
+		List<List<String>> zigZaggedString = new ArrayList<>();
+
+		int verticalIndex = 0;
+		boolean downWard = true;
+
+		if (numRows == 0) {
+			return "";
+		}
+		if (numRows == 1) {
+			return s;
+		}
+
+		for (int i = 0; i < numRows; i++) {
+			zigZaggedString.add(new ArrayList<>());
+		}
+
+		for (int i = 0; i < s.length(); i++) {
+			if (downWard) {
+
+				zigZaggedString.get(verticalIndex).add(s.charAt(i) + "");
+
+				if (verticalIndex != numRows - 1) {
+					verticalIndex++;
+				} else {
+					downWard = false;
+				}
+
+			} else {
+
+				verticalIndex--;
+
+				zigZaggedString.get(verticalIndex).add(s.charAt(i) + "");
+				if (verticalIndex == 0) {
+					downWard = true;
+					verticalIndex++;
+				}
+
+			}
+		}
+
+		for (int i = 0; i < numRows; i++) {
+			for (String zigZaggedChar : zigZaggedString.get(i)) {
+				if (zigZaggedChar != "") {
+					sb.append(zigZaggedChar);
+				}
+			}
+		}
+
+		return sb.toString();
 	}
 
 }
