13d79653cbd923bc2f7cba5b3022532a78576cd1
Tanitsak Jirawuttanakit <tanitsakjirawuttanakit@Tanitsaks-MacBook-Pro.local>
2024-01-26 15:02:52 +0700

    Add IV28

---
+++ src/main/java/solution/IV28_FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,17 @@
+package solution;
+
+public class IV28_FindTheIndexOfTheFirstOccurrenceInAString {
+
+	public static void main(String[] args) {
+		String haystack = "but";
+		String needle = "sad";
+
+		IV28_FindTheIndexOfTheFirstOccurrenceInAString obj = new IV28_FindTheIndexOfTheFirstOccurrenceInAString();
+		obj.strStr(haystack, needle);
+	}
+
+	public int strStr(String haystack, String needle) {
+		return haystack.indexOf(needle);
+	}
+
+}
